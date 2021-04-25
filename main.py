e66aec529125c480eb96e62988c0bd781060f66d
Gabriel Romão <gabriel.s.romao@outlook.com>
2021-04-25 20:55:16 -0300

    the cronometer works

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -8,18 +9,20 @@ from timecontrol import FocusTimer, LazyTimer
 class PomodoroPy(App):
 
     def build(self):
+        # Classes to manage the time
         ftimer = FocusTimer()
         ltimer = LazyTimer()
 
+        # Minimal Layout
         grid = GridLayout(cols=2)
         box = BoxLayout(orientation='vertical')
-
-        button_f = Button(text="Foco", size_hint=(1, .3), on_press=self.start_count)
+        button_f = Button(text="Foco", size_hint=(1, .3), on_press=self._)
         button_l = Button(text="Descanso", size_hint=(1, .3))
         title = Label(text="PomodoroPy", size_hint=(1, .2))
 
-        self.label_f = Label(text=str(ftimer.focus_time))
-        self.label_l = Label(text=str(ltimer.lazy_time))
+        # Adjusting the timer's label
+        self.label_f = Label(text="{} : {}".format(self.convert_time(ftimer.focus_time)[0], self.convert_time(ftimer.focus_time)[1]))
+        self.label_l = Label(text="{} : {}".format(self.convert_time(ltimer.lazy_time)[0], self.convert_time(ltimer.lazy_time)[1]))
 
         box.add_widget(title)
         grid.add_widget(button_f)
@@ -30,9 +33,24 @@ class PomodoroPy(App):
 
         return box
 
-    def start_count(self, button):
-        print("Botão apertado")
-        self.label_f.text = str(int(self.label_f.text) - 1)
+    def _(self, button):
+        if button.state == 'down':
+            Clock.schedule_interval(self.reduce_time, 1)
+
+    # Functions to work the time
+    def convert_time(self, timestr):
+        minutes = int(timestr) // 60
+        seconds = int(timestr) % 60
+        return (minutes, seconds)
+
+    def reconvert_time(self, time):
+        tm = int(time.split(':')[0]) * 60
+        ts = int(time.split(':')[1])
+        return tm + ts
+
+    def reduce_time(self, _):
+        reduced_time = str(self.reconvert_time(self.label_f.text) - 1)
+        self.label_f.text = f"{self.convert_time(reduced_time)[0]} : {self.convert_time(reduced_time)[1]}"
 
 PomodoroPy().run()
 
--- teste.py
+++ teste.py
@@ -1,3 +1,29 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.butto import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+class Programa(App):
+
+    def build(self):
+
+        box = BoxLayout()
+        self.label = Label(text="1")
+        button = Button(text="Aperte", on_press=self.incrementa)
+
+        box.add_widget(button)
+        box.add_widget(self.label)
+
+        return box
+
+    def incrementa(self, button):
+        print(self)
+        print(button)
+        print(button.state)
+        print(type(button.state))
+        self.label.text = str(int(self.label.text) + 1)
+        
+
+
+
+Programa().run()
